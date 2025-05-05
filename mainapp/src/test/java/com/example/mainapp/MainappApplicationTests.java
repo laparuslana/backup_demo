6321c21db93d4af82db5838a794ecd08aab52cfc
laparuslana <rusyalapa@gmail.com>
2025-05-05 18:26:25 +0300

    Tests corrected

--- mainapp/src/test/java/com/example/mainapp/MainappApplicationTests.java
+++ mainapp/src/test/java/com/example/mainapp/MainappApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.mainapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = com.example.mainapp.MainappApplication.class)
 class MainappApplicationTests {
 
     @Test
