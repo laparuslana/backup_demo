57089648ba25ba33a46975356f3afa4c3515a30f
laparuslana <rusyalapa@gmail.com>
2025-05-24 15:33:00 +0300

    Fixed

--- mainapp/src/main/java/com/example/mainapp/Model/Backup/BackupHistoryRepository.java
+++ mainapp/src/main/java/com/example/mainapp/Model/Backup/BackupHistoryRepository.java
@@ -1,10 +1,13 @@
 package com.example.mainapp.Model.Backup;
 
+import com.example.mainapp.Model.UserManagement.MyAppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface BackupHistoryRepository extends JpaRepository<BackupHistory, Long> {
   @Override
-    List<BackupHistory> findAll();
+  List<BackupHistory> findAll();
     
   @Override
     BackupHistory save(BackupHistory backupHistory);
